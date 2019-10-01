6c6807eacfc8efdf69054775c6d3491e53109118
Mike Bédard <mike01bedard@gmail.com>
2019-10-01 17:18:43 -0400

    Grid Generator Commit

---
+++ Assets/Editor/GridGeneratorEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Game
+{
+    
+    [CustomEditor(typeof(GridGenerator))]
+    public class GridGeneratorEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            
+            GridGenerator gridGenerator = (GridGenerator)target;
+            
+            if(GUILayout.Button("Generate Grid"))
+            {
+                gridGenerator.GenerateGrid();
+            }
+            
+            if(GUILayout.Button("Clear Grid"))
+            {
+                gridGenerator.ClearGrid();
+            }
+        }
+    }
+}
