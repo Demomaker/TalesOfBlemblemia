77827dd2044fe04b3d4f2f514e3b3c5766b40a0b
Mike Bédard <mike01bedard@gmail.com>
2019-10-01 17:42:37 -0400

    Character Owner class and subclasses

---
+++ Assets/Scripts/Play/Actors/HumanPlayer.cs
@@ -0,0 +1,15 @@
+﻿namespace Game
+{
+    public class HumanPlayer : CharacterOwner
+    {
+        public override void Win()
+        {
+            base.Win();
+        }
+
+        public override void Lose()
+        {
+            base.Lose();
+        }
+    }
+}
