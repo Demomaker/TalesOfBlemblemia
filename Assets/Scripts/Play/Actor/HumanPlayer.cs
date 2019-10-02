bc206a11449a1e0313813498cfd10eb391d110ed
Mike Bédard <mike01bedard@gmail.com>
2019-10-02 14:55:36 -0400

    Moved Name variable and removed useless base calls in HumanPlayer and ComputerPlayer

--- Assets/Scripts/Play/Actor/HumanPlayer.cs
+++ Assets/Scripts/Play/Actor/HumanPlayer.cs
@@ -2,14 +2,6 @@
 {
     public class HumanPlayer : UnitOwner
     {
-        public override void Win()
-        {
-            //Do nothing
-        }
-
-        public override void Lose()
-        {
-            //Do nothing
-        }
+        //Do nothing
     }
 }
