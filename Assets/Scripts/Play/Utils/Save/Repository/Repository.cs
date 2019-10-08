96e5c4586c30f05ea9ca49b887f46ae90e6208d4
Antoine Lessard <antoinix93@hotmail.com>
2019-10-08 13:25:41 -0400

    Fix bigmess

---
+++ Assets/Scripts/Play/Utils/Save/Repository/Repository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Game
+{
+    public interface Repository<T>
+    {
+        void Insert(T myObject);
+
+        List<T> FindAll();
+
+        void Update(T myObject);
+
+        void Delete(int id);
+    }
+}
