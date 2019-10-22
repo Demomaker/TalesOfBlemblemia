86aaff43b5f3af9e0423aa2e97bffa9b5e0ce7e1
Jérémie Bertrand <jeremie.bertrand@hotmail.com>
2019-10-22 15:03:47 -0400

    Changement des namespaces

--- Assets/Scripts/Play/Controller/CameraController.cs
+++ Assets/Scripts/Play/Controller/CameraController.cs
@@ -1,152 +1,141 @@
-﻿using System;
-using UnityEngine;
-using System.Collections;
- 
-public class CameraController : MonoBehaviour {
- 
-    [SerializeField] private float verticalScrollArea = 10f;
-    [SerializeField] private float horizontalScrollArea = 10f;
-    [SerializeField] private float horizontalMoveSpeed = 10f;
-    [SerializeField] private float verticalMoveSpeed = 10f;
-    [SerializeField] private float zoomSpeed = 10f;
-    private float maxZoom = 8.5f;
-    [SerializeField] private float minZoom = 4.5f;
-    [SerializeField] private float minX = -14f;
-    [SerializeField] private float maxX = 14f;
-    [SerializeField] private float minY = -14f;
-    [SerializeField] private float maxY = 14f;
-    [SerializeField] private KeyCode moveUpKey = KeyCode.W;
-    [SerializeField] private KeyCode moveDownKey = KeyCode.S;
-    [SerializeField] private KeyCode moveLeftKey = KeyCode.A;
-    [SerializeField] private KeyCode moveRightKey = KeyCode.D;
+﻿using UnityEngine;
 
-    public void EnableControls(bool _enable)
-    {
-        if(_enable)
-        {
-            ZoomEnabled = true;
-            MoveEnabled = true;
-        }
-        else
-        {
-            ZoomEnabled = false;
-            MoveEnabled = false;
-        }
-    }
-    public bool ZoomEnabled = true;
-    public bool MoveEnabled = true;
+namespace Game
+{
+    public class CameraController : MonoBehaviour {
+     
+        [SerializeField] private float verticalScrollArea = 10f;
+        [SerializeField] private float horizontalScrollArea = 10f;
+        [SerializeField] private float horizontalMoveSpeed = 10f;
+        [SerializeField] private float verticalMoveSpeed = 10f;
+        [SerializeField] private float zoomSpeed = 10f;
+        private float maxZoom = 8.5f;
+        [SerializeField] private float minZoom = 4.5f;
+        [SerializeField] private float minX = -14f;
+        [SerializeField] private float maxX = 14f;
+        [SerializeField] private float minY = -14f;
+        [SerializeField] private float maxY = 14f;
+        [SerializeField] private KeyCode moveUpKey = KeyCode.W;
+        [SerializeField] private KeyCode moveDownKey = KeyCode.S;
+        [SerializeField] private KeyCode moveLeftKey = KeyCode.A;
+        [SerializeField] private KeyCode moveRightKey = KeyCode.D;
 
-    private Camera camera;
-   
-    private Vector2 _mousePos;
-    private Vector3 _moveVector;
-    private int _xMove;
-    private int _yMove;
-    private int _orthographicSize;
+        public bool ZoomEnabled => zoomEnabled;
+        public bool MoveEnabled => moveEnabled;
 
-    private void Awake()
-    {
-        camera = GetComponent<Camera>();
-    }
-
-    private void Start()
-    {
-        float screenAspect = (float) Screen.width / (float) Screen.height;
-        float camHalfHeight = camera.orthographicSize;
-        float camHalfWidth = screenAspect * camHalfHeight;
-        maxZoom = (maxY - minY) / (camHalfWidth / camHalfHeight) / 2.0f;
-    }
-
-    // Update is called once per frame
-    void Update () {
-        _mousePos = Input.mousePosition;
+        private bool zoomEnabled = true;
+        private bool moveEnabled = true;
+        
+        private Camera camera;
        
-        //Move camera if mouse is at the edge of the screen
-        if(MoveEnabled)
+        private Vector2 mousePos;
+        private Vector3 moveVector;
+        private int xMove;
+        private int yMove;
+        private int orthographicSize;
+
+        private void Awake()
         {
-            if(_mousePos.x < horizontalScrollArea || Input.GetKey(moveLeftKey)) { _xMove = -1; }
-            else if (_mousePos.x >= Screen.width - horizontalScrollArea || Input.GetKey(moveRightKey)) { _xMove = 1; }
-            else { _xMove = 0; }
-               
+            camera = GetComponent<Camera>();
+        }
+
+        private void Start()
+        {
+            var screenAspect = Screen.width / Screen.height;
+            var camHalfHeight = camera.orthographicSize;
+            var camHalfWidth = screenAspect * camHalfHeight;
+            maxZoom = (maxY - minY) / (camHalfWidth / camHalfHeight) / 2.0f;
+        }
+        
+        void Update () {
+            mousePos = Input.mousePosition;
            
-            if(_mousePos.y < verticalScrollArea || Input.GetKey(moveDownKey)) { _yMove = -1; }
-            else if(_mousePos.y >= Screen.height - verticalScrollArea || Input.GetKey(moveUpKey)) { _yMove = 1; }
-            else { _yMove = 0; }
-        }
-        else
-        {
-            _xMove = 0;
-            _yMove = 0;
-        }
-        // Zoom Camera in or out
-        if(ZoomEnabled)
-        {
-            if (Input.GetAxis("Mouse ScrollWheel") < 0) {
-                _orthographicSize = 1;
-            }
-            else if (Input.GetAxis("Mouse ScrollWheel") > 0) {
-                _orthographicSize = -1;
+            //Move camera if mouse is at the edge of the screen
+            if(MoveEnabled)
+            {
+                if(mousePos.x < horizontalScrollArea || Input.GetKey(moveLeftKey)) { xMove = -1; }
+                else if (mousePos.x >= Screen.width - horizontalScrollArea || Input.GetKey(moveRightKey)) { xMove = 1; }
+                else { xMove = 0; }
+                   
+               
+                if(mousePos.y < verticalScrollArea || Input.GetKey(moveDownKey)) { yMove = -1; }
+                else if(mousePos.y >= Screen.height - verticalScrollArea || Input.GetKey(moveUpKey)) { yMove = 1; }
+                else { yMove = 0; }
             }
             else
             {
-                _orthographicSize = 0;
+                xMove = 0;
+                yMove = 0;
             }
-        }
-        else
-        {
-            _orthographicSize = 0;
             
+            // Zoom Camera in or out
+            if(ZoomEnabled)
+            {
+                if (Input.GetAxis("Mouse ScrollWheel") < 0) {
+                    orthographicSize = 1;
+                }
+                else if (Input.GetAxis("Mouse ScrollWheel") > 0) {
+                    orthographicSize = -1;
+                }
+                else
+                {
+                    orthographicSize = 0;
+                }
+            }
+            else
+            {
+                orthographicSize = 0;
+            }
+            
+            Move(xMove, yMove);
+
+            if (orthographicSize != 0)
+            {
+                // Perform zoom
+                camera.orthographicSize = Mathf.Clamp(camera.orthographicSize + orthographicSize * zoomSpeed * Time.deltaTime, minZoom, maxZoom);
+                if (camera.orthographicSize != minZoom && orthographicSize < 0)
+                {
+                    Vector2 screenCenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
+                    float xFactor = Mathf.Abs(mousePos.x - screenCenter.x) / screenCenter.x;
+                    float yFactor = Mathf.Abs(mousePos.y - screenCenter.y) / screenCenter.y;
+                    yMove = xMove = 0;
+                    if (mousePos.x < screenCenter.x) xMove = -1;
+                    else if (mousePos.x > screenCenter.x) xMove = 1;
+                    if (mousePos.y < screenCenter.y) yMove = -1;
+                    else if (mousePos.y > screenCenter.y) yMove = 1;
+                    Move(xMove * xFactor, yMove * yFactor);
+                }
+            }
+            
+            ClampCameraPosition();
+        }
+           
+        private void Move(float x, float y)
+        {
+            moveVector = new Vector3(x * horizontalMoveSpeed, y * verticalMoveSpeed, 0) * Time.deltaTime;
+            transform.Translate(moveVector, Space.World);
+        }
+
+        private void ClampCameraPosition()
+        {
+            Vector3 topRight = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth, camera.pixelHeight, 0));
+            Vector3 bottomLeft = camera.ScreenToWorldPoint(new Vector3(0, 0, 0));
+
+            if (topRight.x > maxX) transform.position = new Vector3(transform.position.x - (topRight.x - maxX), transform.position.y, transform.position.z);
+            if (topRight.y > maxY) transform.position = new Vector3(transform.position.x, transform.position.y - (topRight.y - maxY), transform.position.z);
+            if (bottomLeft.x < minX) transform.position = new Vector3(transform.position.x + (minX - bottomLeft.x), transform.position.y, transform.position.z);
+            if (bottomLeft.y < minY) transform.position = new Vector3(transform.position.x, transform.position.y + (minY - bottomLeft.y), transform.position.z);
         }
         
-        //move the object
-        MoveMe(_xMove, _yMove);
-
-        if (_orthographicSize != 0)
+        public void EnableControls()
         {
-            // Perform zoom
-            camera.orthographicSize = Mathf.Clamp(camera.orthographicSize + _orthographicSize * zoomSpeed * Time.deltaTime, minZoom, maxZoom);
-            if (camera.orthographicSize != minZoom && _orthographicSize < 0)
-            {
-                Vector2 screenCenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
-                float xFactor = Mathf.Abs(_mousePos.x - screenCenter.x) / screenCenter.x;
-                float yFactor = Mathf.Abs(_mousePos.y - screenCenter.y) / screenCenter.y;
-                _yMove = _xMove = 0;
-                if (_mousePos.x < screenCenter.x) _xMove = -1;
-                else if (_mousePos.x > screenCenter.x) _xMove = 1;
-                if (_mousePos.y < screenCenter.y) _yMove = -1;
-                else if (_mousePos.y > screenCenter.y) _yMove = 1;
-                MoveMe(_xMove * xFactor, _yMove * yFactor);
-            }
+            zoomEnabled = moveEnabled = true;
         }
-
-        //  Check if camera is out-of-bounds, if so, move back in-bounds
-        Vector3 topRight = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth, camera.pixelHeight, 0));
-        Vector3 bottomLeft = camera.ScreenToWorldPoint(new Vector3(0, 0, 0));
-
-        if (topRight.x > maxX)
+        
+        public void DisableControls()
         {
-            transform.position = new Vector3(transform.position.x - (topRight.x - maxX), transform.position.y, transform.position.z);
+            zoomEnabled = moveEnabled = false;
         }
-
-        if (topRight.y > maxY)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y - (topRight.y - maxY), transform.position.z);
-        }
-
-        if (bottomLeft.x < minX)
-        {
-            transform.position = new Vector3(transform.position.x + (minX - bottomLeft.x), transform.position.y, transform.position.z);
-        }
-
-        if (bottomLeft.y < minY)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y + (minY - bottomLeft.y), transform.position.z);
-        }
-    }
-       
-    private void MoveMe(float x, float y)
-    {
-        _moveVector = new Vector3(x * horizontalMoveSpeed, y * verticalMoveSpeed, 0) * Time.deltaTime;
-        transform.Translate(_moveVector, Space.World);
     }
 }
+
--- Assets/Scripts/Play/Controller/Cinematic/CinematicController.cs
+++ Assets/Scripts/Play/Controller/Cinematic/CinematicController.cs
@@ -1,99 +1,99 @@
 ﻿using System.Collections;
-using Harmony;
-using Play;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using Finder = Game.Finder;
 
-public class CinematicController : MonoBehaviour
+namespace Game
 {
-    public const float DEFAULT_CAMERA_ZOOM = 8.5f;
-    public const float DEFAULT_CAMERA_Z_POSITION = -10;
-    private Camera camera = null;
-    private Transform camTransform = null;
-    private DialogueManager dialogueManager;
-    private bool isPlayingACutScene = false;
-    private GameObject eventSystemGameObject;
-
-    [SerializeField] private GameObject dialogueUI;
-    [SerializeField] private Transform initialPosition;
-    [SerializeField] private CameraAction[] startActions;
-    [SerializeField] private CameraAction[] endActions;
-
-    public bool IsPlayingACutScene
+    public class CinematicController : MonoBehaviour
     {
-        get
+        public const float DEFAULT_CAMERA_ZOOM = 8.5f;
+        public const float DEFAULT_CAMERA_Z_POSITION = -10;
+        private Camera camera = null;
+        private Transform camTransform = null;
+        private DialogueManager dialogueManager;
+        private bool isPlayingACutScene = false;
+        private GameObject eventSystemGameObject;
+
+        [SerializeField] private GameObject dialogueUI;
+        [SerializeField] private Transform initialPosition;
+        [SerializeField] private CameraAction[] startActions;
+        [SerializeField] private CameraAction[] endActions;
+
+        public bool IsPlayingACutScene
         {
-            return isPlayingACutScene;
-        }
-        private set 
-        { 
-            isPlayingACutScene = value;
-            camera.GetComponent<CameraController>().enabled = eventSystemGameObject.GetComponent<EventSystem>().enabled = !isPlayingACutScene;
-        }
-    }
-
-    private void Awake()
-    {
-        camera = Camera.main;
-        camTransform = camera.gameObject.transform;
-        camTransform.position = initialPosition.position + new Vector3(0,0,DEFAULT_CAMERA_Z_POSITION);
-        camera.orthographicSize = 4;
-        dialogueManager = FindObjectOfType<DialogueManager>();
-        eventSystemGameObject = EventSystem.current.gameObject;
-    }
-
-    private void Start()
-    {
-        if (dialogueUI != null) dialogueUI.SetActive(true);
-        StartCoroutine(PlayCameraActions(startActions));
-    }
-
-    private IEnumerator PlayCameraActions(CameraAction[] cameraActions)
-    {
-        IsPlayingACutScene = true;
-        for (int i = 0; i < cameraActions.Length; i++)
-        {
-            var target = cameraActions[i];
-            var from = camTransform.position;
-            var to = target.CameraTarget.position + new Vector3(0, 0, DEFAULT_CAMERA_Z_POSITION);
-            var duration = target.Duration;
-            
-            for (float t = 0; t < duration; t += Time.deltaTime) {
-                camTransform.position = Vector3.Lerp(from, to, t / duration);
-                yield return null;
-            }
-            
-            camTransform.position = to;
-            dialogueManager.StartDialogue(target.Dialogue);
-            while (dialogueManager.IsDisplayingDialogue)
+            get
             {
-                yield return null;
+                return isPlayingACutScene;
+            }
+            private set 
+            { 
+                isPlayingACutScene = value;
+                camera.GetComponent<Game.CameraController>().enabled = eventSystemGameObject.GetComponent<EventSystem>().enabled = !isPlayingACutScene;
             }
         }
-        var startPos = camTransform.position;
-        var startSize = camera.orthographicSize;
-        var endSize = DEFAULT_CAMERA_ZOOM;
-        float dur = 0.5f;
-        for (float t = 0; t < dur; t += Time.deltaTime) {
-            camTransform.position = Vector3.Lerp(startPos,  new Vector3(0,0, DEFAULT_CAMERA_Z_POSITION), t / dur);
-            camera.orthographicSize = Mathf.Lerp(startSize, endSize, t / dur);
-            yield return null;
+
+        private void Awake()
+        {
+            camera = Camera.main;
+            camTransform = camera.gameObject.transform;
+            camTransform.position = initialPosition.position + new Vector3(0,0,DEFAULT_CAMERA_Z_POSITION);
+            camera.orthographicSize = 4;
+            dialogueManager = FindObjectOfType<DialogueManager>();
+            eventSystemGameObject = EventSystem.current.gameObject;
         }
 
-        camTransform.position = new Vector3(0, 0, DEFAULT_CAMERA_Z_POSITION);
-        camera.orthographicSize = endSize;
+        private void Start()
+        {
+            if (dialogueUI != null) dialogueUI.SetActive(true);
+            StartCoroutine(PlayCameraActions(startActions));
+        }
 
-        IsPlayingACutScene = false;
-    }
+        private IEnumerator PlayCameraActions(CameraAction[] cameraActions)
+        {
+            IsPlayingACutScene = true;
+            for (int i = 0; i < cameraActions.Length; i++)
+            {
+                var target = cameraActions[i];
+                var from = camTransform.position;
+                var to = target.CameraTarget.position + new Vector3(0, 0, DEFAULT_CAMERA_Z_POSITION);
+                var duration = target.Duration;
+                
+                for (float t = 0; t < duration; t += Time.deltaTime) {
+                    camTransform.position = Vector3.Lerp(from, to, t / duration);
+                    yield return null;
+                }
+                
+                camTransform.position = to;
+                dialogueManager.StartDialogue(target.Dialogue);
+                while (dialogueManager.IsDisplayingDialogue)
+                {
+                    yield return null;
+                }
+            }
+            var startPos = camTransform.position;
+            var startSize = camera.orthographicSize;
+            var endSize = DEFAULT_CAMERA_ZOOM;
+            float dur = 0.5f;
+            for (float t = 0; t < dur; t += Time.deltaTime) {
+                camTransform.position = Vector3.Lerp(startPos,  new Vector3(0,0, DEFAULT_CAMERA_Z_POSITION), t / dur);
+                camera.orthographicSize = Mathf.Lerp(startSize, endSize, t / dur);
+                yield return null;
+            }
 
-    public void LaunchCinematic(CameraAction[] actions)
-    {
-        StartCoroutine(PlayCameraActions(actions));
-    }
+            camTransform.position = new Vector3(0, 0, DEFAULT_CAMERA_Z_POSITION);
+            camera.orthographicSize = endSize;
 
-    public void LaunchEndCinematic()
-    {
-        StartCoroutine(PlayCameraActions(endActions));
+            IsPlayingACutScene = false;
+        }
+
+        public void LaunchCinematic(CameraAction[] actions)
+        {
+            StartCoroutine(PlayCameraActions(actions));
+        }
+
+        public void LaunchEndCinematic()
+        {
+            StartCoroutine(PlayCameraActions(endActions));
+        }
     }
 }
