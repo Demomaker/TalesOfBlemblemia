7a1c409183b8d54dbd04170d3af51e7080337c34
Mike Bédard <mike01bedard@gmail.com>
2019-10-28 10:51:26 -0400

    Made audioManager safe to not use

---
+++ Assets/Libraries/Harmony/Scripts/Playmode/Util/Extension/TypeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Harmony
+{
+    public static class TypeExtensions
+    {
+        public static bool IsNot(this Object obj, Type type)
+        {
+            return !(obj.GetType() == type);
+        }
+    }
+}
