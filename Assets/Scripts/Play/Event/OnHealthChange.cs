97b381b1c6a1fd87a2ce717d7c6238deed089d1c
Jérémie Bertrand <jeremie.bertrand@hotmail.com>
2019-11-26 15:12:59 -0500

    UpdatingHealthAndMovement on UnitGraphics

---
+++ Assets/Scripts/Play/Event/OnHealthChange.cs
@@ -0,0 +1,19 @@
+﻿using Harmony;
+
+namespace Game
+{
+    /// <summary>
+    /// OnHurt event channel
+    /// Author : Mike Bédard
+    /// </summary>
+    [Findable(Game.Tags.GAME_EVENT_HANDLER_TAG)]
+    public class OnHealthChange : EventChannel
+    {
+        public event EventHandler Notify;
+
+        public override void Publish()
+        {
+            Notify?.Invoke();
+        }
+    }
+}
