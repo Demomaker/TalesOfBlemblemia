5fed399bc8b29334b67645089973e6126bdbb00a
Antoine Lessard <antoinix93@hotmail.com>
2019-11-29 18:36:26 -0500

    unversioned files

--- Assets/Scripts/Play/Utils/Tags.cs
+++ Assets/Scripts/Play/Utils/Tags.cs
@@ -6,6 +6,6 @@
         public const string SOUND_MANAGER = "SoundManager";
         public const string OVER_WORLD_CONTROLLER_TAG = "OverWorldController";
         public const string GAME_SETTINGS_TAG = "GameSettings";
-        public const string GAME_EVENT_HANDLER_TAG = "GameEventHandler";
+        public const string GAME_EVENT_HANDLER_TAG = "GameEventHandler"; 
     }
 }
